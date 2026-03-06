5f2ee9f44433c95231ba0dde933cccc9d879eb3f
Ian Ribeiro Camargo <ian.camargo@infoles.infra>
2026-03-06 10:29:41 -0300

    feat: responsividade completa v12 - mobile e desktop

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './zucchetti-v11.jsx'
+import App from './zucchetti-v12.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
